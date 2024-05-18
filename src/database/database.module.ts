1176613565f7a8a03d1b34936be29f67d5374def
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2024-05-18 00:37:23 -0300

    feat: add dotenv

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,24 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from 'src/entities/products.entity';
 import { Transaction } from 'src/entities/transactions.entity';
 import { User } from 'src/entities/users.entity';
-import { DataSourceOptions } from 'typeorm';
-
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'store',
-  database: 'store',
-  entities: [Product, Transaction, User],
-  synchronize: true,
-};
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
       useFactory: async () => {
-        return { ...dataSourceOptions };
+        return {
+          type: process.env.DATABASE_TYPE as any,
+          host: process.env.DATABASE_HOST,
+          port: parseInt(process.env.DATABASE_PORT),
+          username: process.env.DATABASE_USERNAME,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE,
+          entities: [Product, Transaction, User],
+          synchronize: true,
+        };
       },
     }),
   ],
